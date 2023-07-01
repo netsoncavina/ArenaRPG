5fdc406762d5a92b1b798d13ed59289588aa46a5
Netson Cavina <netsoncavina@gmail.com>
2023-07-01 01:39:20 -0300

    Desenvolvimento Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { Flex, Image } from "@chakra-ui/react";
 
 const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/ArenaRPGLogo.svg" height="44px" />
+        <Image
+          src="/images/ArenaRPGText.svg"
+          height="44px"
+          display={{ base: "none", md: "unset" }}
+        />
+      </Flex>
+    </Flex>
+  );
 };
 export default Navbar;
