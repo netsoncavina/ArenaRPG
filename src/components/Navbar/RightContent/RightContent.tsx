6015788f2942e611b01a416cd4fa7589ceaf6fca
Netson Cavina <netsoncavina@gmail.com>
2023-07-01 02:19:32 -0300

    Código base RightContent

---
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -0,0 +1,18 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+type RightContentProps = {
+  // user: User;
+};
+
+const RightContent: React.FC<RightContentProps> = () => {
+  return (
+    <>
+      {/* AuthModal */}
+      <Flex justify="center" align="center">
+        {/* Buttons */}
+      </Flex>
+    </>
+  );
+};
+export default RightContent;
