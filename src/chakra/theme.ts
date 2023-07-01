1f401d65164dab4888bb478e2c8319a021d932b6
Netson Cavina <netsoncavina@gmail.com>
2023-07-01 01:04:27 -0300

    Confugração base do tema

---
+++ src/chakra/theme.ts
@@ -0,0 +1,26 @@
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
+
+export const theme = extendTheme({
+  colors: {
+    // Paleta de cores do Chakra UI
+    brand: {
+      100: "#b02b2e",
+    },
+  },
+  fonts: {
+    body: "Open Sans, sans-serif",
+  },
+  styles: {
+    global: () => ({
+      body: {
+        bg: "gray.200",
+      },
+    }),
+  },
+  components: {
+    // Button
+  },
+});
