58ecdf39b214568469eac68c1648e7a024c16d87
Netson Cavina <netsoncavina@gmail.com>
2023-07-01 03:30:10 -0300

    Desenvolvimento dos botões de autenticação

---
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -0,0 +1,19 @@
+import { Flex, Button, Image } from "@chakra-ui/react";
+import React from "react";
+
+const OAuthButtons: React.FC = () => {
+  return (
+    <Flex direction="column" width="100%" mb={4}>
+      <Button variant="oauth" mb={2}>
+        <Image
+          src="/images/googlelogo.png"
+          alt="Google Logo"
+          height="20px"
+          mr={4}
+        />
+        Continue com Google
+      </Button>
+    </Flex>
+  );
+};
+export default OAuthButtons;
