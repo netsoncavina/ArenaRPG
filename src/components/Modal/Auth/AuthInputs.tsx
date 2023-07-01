012eab50d92e66b068bce381250d5af3624e0f62
Netson Cavina <netsoncavina@gmail.com>
2023-07-01 03:30:22 -0300

    Desenvolvimento dos inputs de autenticação

---
+++ src/components/Modal/Auth/AuthInputs.tsx
@@ -0,0 +1,19 @@
+import { authModalState } from "@/src/atoms/AuthModalAtom";
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import { useRecoilValue } from "recoil";
+import Login from "./Login";
+import SignUp from "./SingUp";
+type AuthInputsProps = {};
+
+const AuthInputs: React.FC<AuthInputsProps> = () => {
+  const modalState = useRecoilValue(authModalState);
+
+  return (
+    <Flex direction="column" align="center" width="100%" mt={4}>
+      {modalState.view === "login" && <Login />}
+      {modalState.view === "signup" && <SignUp />}
+    </Flex>
+  );
+};
+export default AuthInputs;
