f4748f7b5318a192f199e04a54d5d8d928dc1762
Netson Cavina <netsoncavina@gmail.com>
2023-08-01 00:31:17 -0300

    Adicionado login com google

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,10 +1,18 @@
-import { Flex, Button, Image } from "@chakra-ui/react";
 import React from "react";
+import { Flex, Button, Image, Text } from "@chakra-ui/react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "@/src/firebase/clientApp";
 
 const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   return (
     <Flex direction="column" width="100%" mb={4}>
-      <Button variant="oauth" mb={2}>
+      <Button
+        variant="oauth"
+        mb={2}
+        isLoading={loading}
+        onClick={() => signInWithGoogle()}
+      >
         <Image
           src="/images/googlelogo.png"
           alt="Google Logo"
@@ -13,6 +21,7 @@ const OAuthButtons: React.FC = () => {
         />
         Continue com Google
       </Button>
+      {error && <Text>{error.message}</Text>}
     </Flex>
   );
 };
