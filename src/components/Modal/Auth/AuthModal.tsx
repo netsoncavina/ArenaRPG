3d3bc3104ddf4801f913b9af51144d65a1045931
Netson Cavina <netsoncavina@gmail.com>
2023-08-01 13:41:07 -0300

    Modal fecha ao logar

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -11,18 +11,26 @@ import {
   Flex,
   Text,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { use, useEffect } from "react";
 import { useRecoilState } from "recoil";
 import AuthInputs from "./AuthInputs";
 import OAuthButtons from "./OAuthButtons";
 import { authModalState } from "@/src/atoms/authModalAtom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/src/firebase/clientApp";
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
+  const [user, loading, error] = useAuthState(auth);
 
   const handleClose = () => {
     setModalState((prev) => ({ ...prev, open: false }));
   };
+
+  useEffect(() => {
+    if (user) handleClose();
+    // console.log(user);
+  }, [user]);
 
   return (
     <>
