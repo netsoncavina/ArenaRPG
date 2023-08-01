4b56c84647c7123589ac7884826deab8ac3e96b8
Netson Cavina <netsoncavina@gmail.com>
2023-08-01 14:19:36 -0300

    Adicionado botão  provisório de Sair

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,17 +1,29 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Button } from "@chakra-ui/react";
 import React from "react";
 import AuthButtons from "./AuthButtons";
+import { signOut } from "firebase/auth";
+import { auth } from "@/src/firebase/clientApp";
 
 type RightContentProps = {
-  // user: User;
+  user: any;
 };
 
-const RightContent: React.FC<RightContentProps> = () => {
+const RightContent: React.FC<RightContentProps> = ({ user }) => {
   return (
     <>
       {/* AuthModal */}
       <Flex justify="center" align="center">
-        <AuthButtons />
+        {user ? (
+          <Button
+            onClick={() => {
+              signOut(auth);
+            }}
+          >
+            Sair
+          </Button>
+        ) : (
+          <AuthButtons />
+        )}
       </Flex>
     </>
   );
