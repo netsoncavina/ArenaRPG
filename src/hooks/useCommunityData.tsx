136b721f1b081f46a08daf3574db5a03afd0bcb3
Netson Cavina <netsoncavina@gmail.com>
2023-08-03 19:14:18 -0300

    Desenvolvimento custom hook

---
+++ src/hooks/useCommunityData.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import {
+  Community,
+  CommunitySnippet,
+  communityState,
+} from "../atoms/communitiesAtom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../firebase/clientApp";
+import { collection, getDocs } from "firebase/firestore";
+
+const useCommunityData = () => {
+  const [user] = useAuthState(auth);
+  const [communityStateValue, setCommunityStateValue] =
+    useRecoilState(communityState);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const onJoinOrLeaveCommunity = (
+    communityData: Community,
+    isJoined: boolean
+  ) => {
+    if (isJoined) {
+      leaveCommunity(communityData.id);
+      return;
+    }
+    joinCommunity(communityData);
+  };
+
+  const getMySnippets = async () => {
+    setLoading(true);
+    try {
+      const snippetDocs = await getDocs(
+        collection(firestore, `users/${user?.uid}/communitySnippets`)
+      );
+
+      const snippets = snippetDocs.docs.map((doc) => ({ ...doc.data() }));
+      setCommunityStateValue((prevState) => ({
+        ...prevState,
+        mySnippets: snippets as CommunitySnippet[],
+      }));
+    } catch (error) {
+      console.log("getMySnippets error", error);
+    }
+    setLoading(false);
+  };
+
+  const joinCommunity = (communityData: Community) => {};
+  const leaveCommunity = (communityId: string) => {};
+
+  useEffect(() => {
+    if (!user) return;
+    getMySnippets();
+  }, [user]);
+
+  return {
+    communityStateValue,
+    onJoinOrLeaveCommunity,
+    loading,
+  };
+};
+export default useCommunityData;
