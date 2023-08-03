4bf9e4cb05a3226c3232e018564f25fbf8be70fa
Netson Cavina <netsoncavina@gmail.com>
2023-08-03 01:38:06 -0300

    Adição do Header

--- src/pages/c/[communityId]/index.tsx
+++ src/pages/c/[communityId]/index.tsx
@@ -5,6 +5,7 @@ import { doc, getDoc } from "firebase/firestore";
 import { GetServerSidePropsContext } from "next";
 import safeJsonStringify from "safe-json-stringify";
 import NotFound from "@/src/components/Community/NotFound";
+import Header from "@/src/components/Community/Header";
 
 type CommunityPageProps = {
   communityData: Community;
@@ -12,7 +13,11 @@ type CommunityPageProps = {
 
 const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
   if (!communityData) return <NotFound />;
-  return <div>Bem vindo a {communityData.id}</div>;
+  return (
+    <>
+      <Header communityData={communityData} />
+    </>
+  );
 };
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
