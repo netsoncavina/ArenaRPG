b9f3b02a9c07d20e690b2a48ed687ce723bdda5b
Netson Cavina <netsoncavina@gmail.com>
2023-08-07 15:01:03 -0300

    Adição de validação de usuario

--- src/pages/c/[communityId]/submit.tsx
+++ src/pages/c/[communityId]/submit.tsx
@@ -3,15 +3,18 @@ import { Text, Box } from "@chakra-ui/react";
 
 import PageContent from "@/src/components/Layout/PageContent";
 import NewPostForm from "@/src/components/Posts/NewPostForm";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/src/firebase/clientApp";
 
 const SubmitPostPage: React.FC = () => {
+  const [user] = useAuthState(auth);
   return (
     <PageContent>
       <>
         <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
           <Text>Criar Post</Text>
         </Box>
-        <NewPostForm />
+        {user && <NewPostForm user={user} />}
       </>
       <>About</>
     </PageContent>
