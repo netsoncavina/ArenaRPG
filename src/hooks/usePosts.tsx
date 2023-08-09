733fc1447c21184bbe876b9299d65a993a3fd3fe
Netson Cavina <netsoncavina@gmail.com>
2023-08-08 21:24:30 -0300

    Desenvolvimento função DeletePost

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useRecoilState } from "recoil";
-import { postState } from "../atoms/postsAtom";
+import { Post, postState } from "../atoms/postsAtom";
+import { firestore, storage } from "../firebase/clientApp";
+import { ref, deleteObject } from "firebase/storage";
+import { deleteDoc, doc } from "firebase/firestore";
 
 const usePosts = () => {
   const [postStateValue, setPostStateValue] = useRecoilState(postState);
@@ -9,7 +12,25 @@ const usePosts = () => {
 
   const onSelectPost = async () => {};
 
-  const onDeletePost = async () => {};
+  const onDeletePost = async (post: Post): Promise<boolean> => {
+    try {
+      if (post.imageUrl) {
+        const imageRef = ref(storage, `posts/${post.id}/image`);
+        await deleteObject(imageRef);
+      }
+
+      const postDocRef = doc(firestore, "posts", post.id!);
+      await deleteDoc(postDocRef);
+
+      setPostStateValue((prevState) => ({
+        ...prevState,
+        posts: prevState.posts.filter((p) => p.id !== post.id),
+      }));
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
 
   return {
     postStateValue,
