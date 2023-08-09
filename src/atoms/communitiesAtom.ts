814bbf528bd3a7a3a112626d50c39bbf0d23cf11
Netson Cavina <netsoncavina@gmail.com>
2023-08-09 10:48:39 -0300

    Adição de current community ao communityState

--- src/atoms/communitiesAtom.ts
+++ src/atoms/communitiesAtom.ts
@@ -18,7 +18,7 @@ export interface CommunitySnippet {
 
 interface CommunityState {
   mySnippets: CommunitySnippet[];
-  // visitedCommunities: Community[];
+  currentCommunity?: Community;
 }
 
 const defaultCommunityState: CommunityState = {
