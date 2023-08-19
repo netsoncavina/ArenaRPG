77d0bc0d17e467c4f99f7d210d673470a3ffd391
Netson Cavina <netsoncavina@gmail.com>
2023-08-18 22:07:52 -0300

    Adição da lógica de like/deslike

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,14 +1,98 @@
 import React from "react";
 import { useRecoilState } from "recoil";
-import { Post, postState } from "../atoms/postsAtom";
-import { firestore, storage } from "../firebase/clientApp";
+import { Post, PostVote, postState } from "../atoms/postsAtom";
+import { auth, firestore, storage } from "../firebase/clientApp";
 import { ref, deleteObject } from "firebase/storage";
-import { deleteDoc, doc } from "firebase/firestore";
+import { collection, deleteDoc, doc, writeBatch } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const usePosts = () => {
+  const [user] = useAuthState(auth)
   const [postStateValue, setPostStateValue] = useRecoilState(postState);
 
-  const onVote = async () => {};
+  const onVote = async (post: Post, vote: number, communityId: string) => {
+    // Check for user, if not, open auth modal
+    try {
+      const {voteCount} = post;
+      const existingVote = postStateValue.postVotes.find(
+        (vote) => vote.postId === post.id
+      )
+      const batch = writeBatch(firestore);
+      // Create copys of states to update later
+      const updatedPost = {...post}
+      const updatedPosts = [...postStateValue.posts]
+      let updatedPostVotes = [...postStateValue.postVotes]
+      let voteChange = vote;
+
+
+      if (!existingVote){
+        // Create a new vote
+        const postVoteRef = doc(collection(firestore, 'users', `${user?.uid}/postVotes`))
+
+        const newVote: PostVote = {
+          id: postVoteRef.id,
+          postId: post.id!,
+          communityId,
+          voteValue: vote
+        }
+
+        batch.set(postVoteRef, newVote)
+
+        updatedPost.voteCount = voteCount + vote
+        updatedPostVotes = [...updatedPostVotes, newVote]
+
+      } else {
+        // Update existing vote
+        const postVoteRef = doc(firestore, 'users', `${user?.uid}/postVotes/${existingVote.id}`)
+
+        // If vote is the same as existing vote, delete vote +1/-1
+        if (existingVote.voteValue === vote) {
+          updatedPost.voteCount = voteCount - vote
+          updatedPostVotes = updatedPostVotes.filter(vote => vote.id !== existingVote.id)
+          
+          // Delete vote
+          batch.delete(postVoteRef)
+          voteChange *= -1
+
+          // Change vote, update vote +2/-2
+        } else {
+          updatedPost.voteCount = voteCount + (vote * 2)
+
+          const voteIndex = postStateValue.postVotes.findIndex(vote => vote.id === existingVote.id)
+
+          updatedPostVotes[voteIndex] = {
+            ...existingVote,
+            voteValue: vote
+          }
+
+          // Update vote
+          batch.update(postVoteRef, {
+            voteValue: vote
+          })
+
+          voteChange = 2 * vote 
+        }
+      }
+
+      // Update post document
+      const postRef = doc(firestore, 'posts', post.id)
+      batch.update(postRef, {voteCount: voteCount + voteChange})
+
+      await batch.commit()
+
+      // Update post state
+      const postIndex = updatedPosts.findIndex(item => item.id === post.id)
+      updatedPosts[postIndex] = updatedPost
+      setPostStateValue((prevState) => ({
+        ...prevState,
+        posts: updatedPosts,
+        postVotes: updatedPostVotes
+      }))
+
+    } catch (error) {
+      console.log("OnVote Error: ", error);
+    }
+  };
 
   const onSelectPost = async () => {};
 
