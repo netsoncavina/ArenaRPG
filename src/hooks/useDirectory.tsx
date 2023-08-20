8e707bd30c07795ac08517157b6aa7a5213ee4a0
Netson Cavina <netsoncavina@gmail.com>
2023-08-20 20:10:41 -0300

    Adição de useEffect

--- src/hooks/useDirectory.tsx
+++ src/hooks/useDirectory.tsx
@@ -1,15 +1,18 @@
-import React from "react";
-import { useRecoilState } from "recoil";
+import React, { useEffect } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
 import {
   DirectoryMenuItem,
   directoryMenuState,
 } from "../atoms/directoryMenuAtom";
 import { useRouter } from "next/router";
+import { communityState } from "../atoms/communitiesAtom";
+import { FaReddit } from "react-icons/fa";
 
 const useDirectory = () => {
   const [directoryState, setDirectoryState] =
     useRecoilState(directoryMenuState);
   const router = useRouter();
+  const communityStateValue = useRecoilValue(communityState);
 
   const onSelectMenuItem = (menuItem: DirectoryMenuItem) => {
     setDirectoryState((prev) => ({
@@ -25,6 +28,23 @@ const useDirectory = () => {
   const toggleMenuOpen = () => {
     setDirectoryState((prev) => ({ ...prev, isOpen: !directoryState.isOpen }));
   };
+
+  useEffect(() => {
+    const { currentCommunity } = communityStateValue;
+    if (currentCommunity) {
+      setDirectoryState((prev) => ({
+        ...prev,
+        selectedMenuItem: {
+          displayText: `c/${currentCommunity.id}`,
+          link: `/c/${currentCommunity.id}`,
+          imageUrl: currentCommunity.ImageUrl,
+          icon: FaReddit,
+          iconColor: "blue.500",
+        },
+      }));
+    }
+  }, [communityStateValue.currentCommunity]);
+
   return { directoryState, toggleMenuOpen, onSelectMenuItem };
 };
 export default useDirectory;
