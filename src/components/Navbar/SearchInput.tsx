1ec67b3540f0385d2a8554d3b30a1641e6b53830
Netson Cavina <netsoncavina@gmail.com>
2023-08-25 20:58:09 -0300

    Mudança de design

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -8,27 +8,48 @@ type SearchInputProps = {
 };
 
 const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
+  const [visible, setVisible] = React.useState(false);
   return (
-    <Flex flexGrow={1} maxWidth={user ? "auto" : "600px"} mr={2} align="center">
+    <Flex flexGrow={1} maxWidth="200px" mr={2} align="center">
       <InputGroup>
-        <InputLeftElement pointerEvents="none">
-          <SearchIcon color="gray.300" mb={1} />
+        <InputLeftElement>
+          <SearchIcon
+            color="primary"
+            boxSize={6}
+            mb={1}
+            onClick={() => setVisible(!visible)}
+          />
         </InputLeftElement>
         <Input
           placeholder="Pesquisar"
           fontSize="10pt"
           _placeholder={{ color: "gray.500" }}
-          _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
-          _focus={{
-            outline: "none",
-            border: "1px solid",
-            borderColor: "blue.500",
-          }}
+          // _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
+          _focus={
+            visible
+              ? {
+                  outline: "none",
+                  border: "1px solid",
+                  borderColor: "blue.500",
+                }
+              : {
+                  outline: "none",
+                  border: "none",
+                  borderColor: "blue.500",
+                }
+          }
           height="34px"
-          bg="gray.50"
+          bg={visible ? "secondary" : "transparent"}
+          border={visible ? "1px solid" : "none"}
+          borderColor="primary"
+          width={visible ? "150px" : "0px"}
+          transition="width 0.5s ease-in-out"
         />
       </InputGroup>
     </Flex>
   );
 };
+
+const styles = {};
+
 export default SearchInput;
