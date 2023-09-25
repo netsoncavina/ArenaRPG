8d57336f24e310fc762bc15b89e03837478ff3bd
Netson Cavina <netsoncavina@gmail.com>
2023-09-25 10:57:57 -0300

    Mudança de estilo

--- src/components/Community/CreatePostLink.tsx
+++ src/components/Community/CreatePostLink.tsx
@@ -1,6 +1,6 @@
 import { authModalState } from "@/src/atoms/authModalAtom";
 import { auth } from "@/src/firebase/clientApp";
-import { Flex, Icon, Input } from "@chakra-ui/react";
+import { Flex, Icon, Input, Image } from "@chakra-ui/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
@@ -35,45 +35,45 @@ const CreatePostLink: React.FC<CreatePostProps> = () => {
     <Flex
       justify="space-evenly"
       align="center"
-      bg="white"
+      bg="secondary"
       height="56px"
       borderRadius={4}
-      border="1px solid"
-      borderColor="gray.300"
+      // border="1px solid"
+      borderColor="primary"
       p={2}
       mb={4}
     >
-      <Icon as={FaReddit} fontSize={36} color="gray.300" mr={4} />
+      <Image src="/images/arena_rpg_icone.png" height="55px" />
       <Input
-        placeholder="Create Post"
+        placeholder="Criar Post"
         fontSize="10pt"
-        _placeholder={{ color: "gray.500" }}
+        _placeholder={{ color: "gray.200" }}
         _hover={{
-          bg: "white",
+          // bg: "white",
           border: "1px solid",
-          borderColor: "blue.500",
+          borderColor: "primary_hover",
         }}
         _focus={{
           outline: "none",
-          bg: "white",
+          bg: "secondary",
           border: "1px solid",
-          borderColor: "blue.500",
+          borderColor: "primary_hover",
         }}
-        bg="gray.50"
-        borderColor="gray.200"
+        bg="secondary"
+        borderColor="primary"
         height="36px"
         borderRadius={4}
         mr={4}
         onClick={onClick}
       />
-      <Icon
+      {/* <Icon
         as={IoImageOutline}
         fontSize={24}
         mr={4}
-        color="gray.400"
+        color="primary"
         cursor="pointer"
       />
-      <Icon as={BsLink45Deg} fontSize={24} color="gray.400" cursor="pointer" />
+      <Icon as={BsLink45Deg} fontSize={24} color="primary" cursor="pointer" /> */}
     </Flex>
   );
 };
